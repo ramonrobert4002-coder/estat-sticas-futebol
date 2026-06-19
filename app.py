ec7aa0b2c6ce36532411affd140797d5ad31b12b
ramonrobert4002-coder <ramonrobert4002@gmail.com>
2026-06-19 11:42:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,26 +14,23 @@ headers = {
 def home():
     return """
     <h1>Estatísticas de Futebol</h1>
+    <p>Use: /test</p>
     <p>Use: /competitions</p>
-    <p>Use: /team?id=ID_DO_TIME</p>
+    <p>Use: /brasileirao</p>
     """
 
-# TESTE DA API
 @app.route("/test")
 def test():
     url = f"{BASE_URL}/competitions"
     r = requests.get(url, headers=headers)
     return r.json()
 
-# LISTAR COMPETIÇÕES (pra você pegar IDs corretos)
 @app.route("/competitions")
 def competitions():
     url = f"{BASE_URL}/competitions"
     r = requests.get(url, headers=headers)
     return r.json()
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
 @app.route("/brasileirao")
 def brasileirao():
     url = f"{BASE_URL}/competitions/2013/matches?limit=10"
@@ -45,4 +42,8 @@ def brasileirao():
     return {
         "jogos_encontrados": len(matches),
         "jogos": matches
+    }
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
     }
