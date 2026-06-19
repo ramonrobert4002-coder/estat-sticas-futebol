62ba383edb3ea151bad6b5ed509ff496a2e8fc8f
ramonrobert4002-coder <ramonrobert4002@gmail.com>
2026-06-19 10:09:16 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(_name_)
+
+@app.route("/")
+def home():
+    return """
+    <h1>Estatísticas de Futebol</h1>
+    <p>Aplicativo criado por Ramon.</p>
+    <p>Em breve: médias de gols, over 2.5 e BTTS.</p>
+    """
+
+if _name_ == "_main_":
+    app.run(host="0.0.0.0", port=10000)
